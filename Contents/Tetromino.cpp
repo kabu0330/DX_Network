0318e8221f9fbde75ff64c8e0215576d89923819
성민 류 <baekhun0330@naver.com>
2025-06-01 23:41:49 +0900

    테트로미노 세팅

--- Contents/Tetromino.cpp
+++ Contents/Tetromino.cpp
@@ -6,12 +6,17 @@
 ATetromino::ATetromino()
 {
 	CurFramePacketTime = 20.0f * 30.0f;
-	BodyRenderer->CreateAnimation("Mino", "minos00.png", 0, 0);
-	BodyRenderer->ChangeAnimation("Mino");
 	BodyRenderer->SetActive(false);
 
+	CreateRenderers();	
+	InitType();
+	SetType(MinoType);
+}
+
+void ATetromino::CreateRenderers()
+{
 	SetActorRelativeScale3D(FVector(1.0f, 1.0f));
-	
+
 	MinoRenders.resize(4);
 	for (size_t i = 0; i < MinoRenders.size(); i++)
 	{
@@ -25,22 +30,136 @@ ATetromino::ATetromino()
 			USpriteRenderer* NewMinoRenderer = CreateDefaultSubObject<USpriteRenderer>().get();
 			NewMinoRenderer->SetupAttachment(RootComponent);
 			NewMinoRenderer->SetAutoScaleRatio(0.5f);
-			NewMinoRenderer->CreateAnimation("Mino", "minos00.png", 0, 0);
-			NewMinoRenderer->ChangeAnimation("Mino");
+			NewMinoRenderer->CreateAnimation("Block", "minos00.png", 0, 0);
+			NewMinoRenderer->CreateAnimation("J_Mino", "minos00.png", 1, 1);
+			NewMinoRenderer->CreateAnimation("S_Mino", "minos00.png", 2, 2);
+			NewMinoRenderer->CreateAnimation("L_Mino", "minos00.png", 3, 3);
+			NewMinoRenderer->CreateAnimation("T_Mino", "minos00.png", 4, 4);
+			NewMinoRenderer->CreateAnimation("Z_Mino", "minos00.png", 5, 5);
+			NewMinoRenderer->CreateAnimation("I_Mino", "minos00.png", 6, 6);
+			NewMinoRenderer->CreateAnimation("O_Mino", "minos00.png", 7, 7);
+			NewMinoRenderer->ChangeAnimation("S_Mino");
 			FVector Scale = NewMinoRenderer->GetCurScale();
 
-			float XPos = Scale.X * static_cast<float>(x) - Scale.X;
-			float YPos = Scale.Y - (static_cast<float>(y) * Scale.Y);
+			float XPos = (Scale.X * static_cast<float>(x)) - (Scale.X * 1.5f);
+			float YPos = (Scale.Y * 1.0f) - (static_cast<float>(y) * Scale.Y);
 			NewMinoRenderer->SetRelativeLocation({ XPos, YPos });
 			FVector Pos = NewMinoRenderer->GetRelativeLocation();
 			MinoRenders[y][x] = NewMinoRenderer;
 		}
 	}
-	
 }
 
-ATetromino::~ATetromino()
+void ATetromino::InitType()
 {
+	for (size_t y = 0; y < MinoRenders.size(); y++)
+	{
+		for (size_t x = 0; x < MinoRenders[0].size(); x++)
+		{
+			MinoRenders[y][x]->SetActive(false);
+		}
+	}
+}
+
+void ATetromino::SetType(EMinoType _Type)
+{
+	InitType();
+
+	switch (_Type)
+	{
+	case EMinoType::I_MINO:
+		for (size_t y = 0; y < MinoRenders.size(); y++)
+		{
+			MinoRenders[y][1]->ChangeAnimation("I_MINO");
+			MinoRenders[y][1]->SetActive(true);
+		}
+		break;
+	case EMinoType::O_MINO:
+		for (int y = 1; y <= 2; y++)
+		{
+			for (int x = 1; x <= 2; x++)
+			{
+				MinoRenders[y][x]->ChangeAnimation("O_MINO");
+				MinoRenders[y][x]->SetActive(true);
+
+			}
+		}
+		break;
+	case EMinoType::T_MINO:
+		for (int y = 1; y <= 2; y++)
+		{
+			for (int x = 0; x <= 2; x++)
+			{
+				if ((2 == y && 0 == x) || (2 == y && 2 == x))
+				{
+					continue;
+				}
+				MinoRenders[y][x]->ChangeAnimation("T_MINO");
+				MinoRenders[y][x]->SetActive(true);
+			}
+		}
+		break;
+	case EMinoType::J_MINO:
+		for (int y = 0; y <= 2; y++)
+		{
+			for (int x = 1; x <= 2; x++)
+			{
+				if ((0 == y && 1 == x) || (1 == y && 1 == x))
+				{
+					continue;
+				}
+				MinoRenders[y][x]->ChangeAnimation("J_MINO");
+				MinoRenders[y][x]->SetActive(true);
+			}
+		}
+		break;
+	case EMinoType::L_MINO:
+		for (int y = 0; y <= 2; y++)
+		{
+			for (int x = 1; x <= 2; x++)
+			{
+				if ((0 == y && 2 == x) || (1 == y && 2 == x))
+				{
+					continue;
+				}
+				MinoRenders[y][x]->ChangeAnimation("L_MINO");
+				MinoRenders[y][x]->SetActive(true);
+			}
+		}
+		break;
+	case EMinoType::S_MINO:
+		for (int y = 1; y <= 2; y++)
+		{
+			for (int x = 0; x <= 2; x++)
+			{
+				if ((1 == y && 0 == x) || (2 == y && 2 == x))
+				{
+					continue;
+				}
+				MinoRenders[y][x]->ChangeAnimation("S_MINO");
+				MinoRenders[y][x]->SetActive(true);
+			}
+		}
+		break;
+	case EMinoType::Z_MINO:
+		for (int y = 1; y <= 2; y++)
+		{
+			for (int x = 0; x <= 2; x++)
+			{
+				if ((1 == y && 2 == x) || (2 == y && 0 == x))
+				{
+					continue;
+				}
+				MinoRenders[y][x]->ChangeAnimation("Z_MINO");
+				MinoRenders[y][x]->SetActive(true);
+			}
+		}
+		break;
+	case EMinoType::MAX:
+		break;
+	default:
+		break;
+	}
 }
 
 void ATetromino::BeginPlay()
@@ -76,7 +195,7 @@ void ATetromino::SetControllMode(float _DeltaTime)
 	}
 	if (true == UEngineInput::IsDown('Q'))
 	{
-		AddActorRotation(FVector(0.0f, 0.0f, 30.0f));
+		AddActorRotation(FVector(0.0f, 0.0f, 90.0f));
 		FVector Rotation = GetActorRotation();
 	}
 
--- Contents/Tetromino.h
+++ Contents/Tetromino.h
@@ -1,21 +1,40 @@
 #pragma once
 #include "ServerPawn.h"
 
+enum class EMinoType : __int8
+{
+	I_MINO,
+	O_MINO,
+	T_MINO,
+	J_MINO,
+	L_MINO,
+	S_MINO,
+	Z_MINO,
+	MAX
+};
+
 // Ό³Έν :
 class ATetromino : public AServerPawn
 {
 public:
 	ATetromino();
-	~ATetromino();
+	~ATetromino() {};
+
+	void SetType(EMinoType _Type);
 
 protected:
 	void BeginPlay() override;
 	void Tick(float _DeltaTime)	override;
 
 	void SetControllMode(float _DeltaTime) override;
+	
 
 private:
+	void CreateRenderers();
+	void InitType();
+
 	std::vector<std::vector<USpriteRenderer*>> MinoRenders;
+	EMinoType MinoType = EMinoType::Z_MINO;
 
 private:
 	// delete Function
